c5834e7200b00e15a17fac276a6b33473aa0449f
Esumandi <sumandieugene@gmail.com>
2024-08-24 20:19:33 +0800

    eugene

---
+++ program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 20;
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
